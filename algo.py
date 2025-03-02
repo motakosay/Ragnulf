c04b145b96ae8ab1f060ddd57a5688ce2812a197
Yahia Kamal <93154332+motakosay@users.noreply.github.com>
2025-03-03 00:48:38 +0200

    Update algo.py

--- algo.py
+++ algo.py
@@ -36,7 +36,7 @@ from utils import croix_valide, ftl_valide, cfop_valide
 from stats import *
 from utils import croix_valide, ftl_valide, cfop_valide, replace_sublist
 
-SHORTCUTS = "shortcuts.json"
+SHORTCUTS = "/content/Ragnulf/shortcuts.json"
 
 def algo_cfop(c):
     '''
