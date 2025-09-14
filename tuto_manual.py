b1635d74cdfa066c8e4be545b0a1fcf6d46ca5e2
Ironside <93154332+motakosay@users.noreply.github.com>
2025-09-14 11:34:58 +0300

    Update tuto_manual.py

--- tuto_manual.py
+++ tuto_manual.py
@@ -2,7 +2,7 @@
 import os
 
 def get_key():
-    key = input("Type LEFT, RIGHT or EXIT ").strip().upper()
+    key = input("Type LEFT, RIGHT or EXIT:").strip().upper()
     if key in ("LEFT", "RIGHT", "EXIT"):
         return key
     return "OTHER"
@@ -23,13 +23,13 @@ def tuto_manual():
     while True:
         os.system('cls' if os.name == 'nt' else 'clear')
         print(f"{steps[idx]}\n")
-        print("Type LEFT ← | RIGHT → | ENTER to exit")
+        print("Type LEFT ← | RIGHT → | EXIT to exit")
 
         key = get_key()
         if key == "LEFT" and idx > 0:
             idx -= 1
         elif key == "RIGHT" and idx < len(steps) - 1:
             idx += 1
-        elif key == "ENTER":
+        elif key == "EXIT":
             print("\nTutorial finished. 🎉")
             break
